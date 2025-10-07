758c6d1b24b2d9be854c1dff468268ee55194fb4
mohideen143 <smohideen2002@gmail.com>
2025-10-07 18:26:14 +0530

    update UserController.java create getAllUsers Api

--- src/main/java/com/Auth/authenticationAndAuthorization/Controller/UserController.java
+++ src/main/java/com/Auth/authenticationAndAuthorization/Controller/UserController.java
@@ -1,13 +1,17 @@
 package com.Auth.authenticationAndAuthorization.Controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Auth.authenticationAndAuthorization.Dto.UserDto;
+import com.Auth.authenticationAndAuthorization.Entity.User;
 import com.Auth.authenticationAndAuthorization.Service.UserService;
 
 @RestController
@@ -30,6 +34,12 @@ public class UserController {
 	@PostMapping("/Login")
 	public ResponseEntity<?> userLogin(@RequestBody UserDto userDto){
 		return userService.userLogin(userDto);
+		
+	}
+	
+	@GetMapping("/getAllUsers")
+	public ResponseEntity<List<UserDto>> getAllUsers(){
+		return userService.getAllUsers();
 		
 	}
 	
